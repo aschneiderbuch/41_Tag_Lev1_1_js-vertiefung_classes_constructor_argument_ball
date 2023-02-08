42ec2a45927e47e58132219b916f20147d0cb148
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-08 11:22:25 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ console.log("test");
 
 class Ball {
     constructor(ballType = "leer-->regular"){
-        this.ballType = ballType;
+        this.ballType = ballType;   // = ballType wird von oben geholt
         }
     }
 
@@ -13,3 +13,17 @@ ball2 = new Ball("super");
 
 console.log(ball1.ballType);	//=> "regular"
 console.log(ball2.ballType);	//=> "super"
+
+
+
+// oder
+function Ball2(ballType = "leer-->regular"){
+    this.ballType = ballType;    // = ballType wird von oben geholt
+}
+
+
+ball11 = new Ball2();
+ball21 = new Ball2("super");
+
+console.log(ball11.ballType);	//=> "regular"
+console.log(ball21.ballType);
