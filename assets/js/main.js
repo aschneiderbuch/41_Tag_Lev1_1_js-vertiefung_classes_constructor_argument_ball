d514d4b7d87b126d201b9bf19ffa629644e60047
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-08 11:20:25 +0100

    leer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+class Ball {
+    constructor(ballType = "leer-->regular"){
+        this.ballType = ballType;
+        }
+    }
+
+
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+console.log(ball1.ballType);	//=> "regular"
+console.log(ball2.ballType);	//=> "super"
